b9374e262cacf98a78b2b2d198c125591ac881e0
mscbr <scibor.maciej@gmail.com>
2020-04-09 14:09:14 +0200

    typography init

--- src/themming/typography.ts
+++ src/themming/typography.ts
@@ -1,1 +1,23 @@
-export const typography = { fontFamily: 'Hind' };
+import { pxToRem } from 'shared/helpers';
+
+export const typography = {
+  fontFamily: {
+    hind: '"Hind", sans-serif',
+    playfairDisplay: '"Playfair Display", serif',
+  },
+  fontWeight: {
+    light: '300',
+    regular: '400',
+    medium: '500',
+    semiBold: '600',
+    bold: '700',
+    black: '900',
+  },
+  fontSize: {
+    18: pxToRem(18),
+    16: pxToRem(16),
+  },
+  letterSpacing: {
+    1: pxToRem(1),
+  },
+};
