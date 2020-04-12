eb41b0baa2bd56d4cdc370d8961708bf061a5b9c
mscbr <scibor.maciej@gmail.com>
2020-04-12 20:20:38 +0200

    Solves dropdown

---
+++ src/components/dropdown/parts/userLabel.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import theme from 'themming';
+
+const StyledSpan = styled.span`
+  color: ${theme.palette.dropdown};
+  font-family: ${theme.typography.fontFamily.hind};
+  font-size: ${theme.typography.fontSize[14]};
+  letter-spacing: 0.7px;
+`;
+
+const UserLabel = (props: { children?: string }) => {
+  return <StyledSpan>{props.children}</StyledSpan>;
+};
+
+export default UserLabel;
